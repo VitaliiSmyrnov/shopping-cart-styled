c5d17c7268c2f3a9f1d2be3bc16833be7f69befe
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-08-18 18:28:11 +0300

    feat: add Header Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,33 @@
 import React from "react";
 
+import { FooterStyled } from "./Footer.styled";
+
 export const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <FooterStyled>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/VitaliiSmyrnov"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>{" "}
+          &bull;{" "}
+          <a
+            href="https://www.linkedin.com/in/vitalii-smyrnov/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedIn
+          </a>
+        </li>
+
+        <li> &copy; 2022 - {currentYear}. All rights reserved.</li>
+      </ul>
+    </FooterStyled>
+  );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { HeaderStyled, LogoWrapper, CartWrapper } from "./Header.styled";
+
+import logo from "src/assets/svg/shopping-bags.svg";
+import shoppingCart from "src/assets/svg/shopping-cart.svg";
 
 export const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderStyled>
+      <Link to="/">
+        <LogoWrapper>
+          <img src={logo} alt="Logo" />
+        </LogoWrapper>
+      </Link>
+
+      <Link to="/cart">
+        <CartWrapper>
+          <img src={shoppingCart} alt="Shopping cart" />
+          <span>3</span>
+        </CartWrapper>
+      </Link>
+    </HeaderStyled>
+  );
 };
