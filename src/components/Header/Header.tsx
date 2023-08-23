a7d6a74220cb7c9b72e00ca756d17662c0b50acd
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-08-23 16:07:03 +0300

    feat: add productsCount to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useLocalStorageState from "use-local-storage-state";
 
 import { HeaderStyled, CartWrapper } from "./Header.styled";
 
 import logo from "src/assets/svg/shopping-bags.svg";
 import shoppingCart from "src/assets/svg/shopping-cart.svg";
 
+import { CartProps } from "src/modules/IProducts";
+
 export const Header: React.FC = () => {
+  const [cart] = useLocalStorageState<CartProps>("cart");
+
+  const productsCount: number = Object.keys(cart || {}).length;
+
   return (
     <HeaderStyled>
       <Link to="/">
-     
-          <img src={logo} alt="Logo" />
- 
+        <img src={logo} alt="Logo" />
       </Link>
 
       <Link to="/cart">
         <CartWrapper>
           <img src={shoppingCart} alt="Shopping cart" />
-          <span>3</span>
+          <span>{productsCount}</span>
         </CartWrapper>
       </Link>
     </HeaderStyled>
